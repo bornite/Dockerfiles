051cb05fdcf412686aa42b731452e35fdcfd5ec3
bornite <tmkoikee@gmail.com>
2021-01-30 00:09:27 +0900

    :tada: Add install process.

--- alpine-texlive-ja/build.sh
+++ alpine-texlive-ja/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+docker pull paperist/alpine-texlive-ja
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $1.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $1.dvi
