2e4696bf0d4962a3b0a85bb44a41d1bfd7677d44
bornite <tmkoikee@gmail.com>
2021-04-13 19:58:17 +0900

    :fire: Delete working files.

--- build-texlive-ja/build.sh
+++ build-texlive-ja/build.sh
@@ -3,3 +3,7 @@
 docker pull paperist/alpine-texlive-ja
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $1.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $1.dvi
+
+rm -f $1.aux
+rm -f $1.dvi
+rm -f $1.log
