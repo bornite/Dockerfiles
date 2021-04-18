6c3be8abb95c04c592ed9d798b6589bc752b5da8
bornite <tmkoikee@gmail.com>
2021-04-18 11:25:07 +0900

    :fire: Delete build-texlive-ja

--- build-texlive-ja/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull paperist/alpine-texlive-ja
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $1.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $1.dvi
-
-rm -f $1.aux
-rm -f $1.dvi
-rm -f $1.log
